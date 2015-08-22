95e00890d1681cb3ab8973024d7745a1a3a56666
Csaba Csete <csaba.csete@gmail.com>
2015-08-22 16:44:22 +0300

    Worked on FootballScores
    -added content description
    -added Simple Widget
    -created API key
    -modified the League IDs
    -updated UI

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/AppWidgetConfigure.java
@@ -0,0 +1,40 @@
+package barqsoft.footballscores.widget;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.R;
+
+/**
+ * Created by Csabi on 22-Aug-15.
+ */
+public class AppWidgetConfigure extends Activity {
+
+    private int mAppWidgetId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+
+            RemoteViews views = new RemoteViews(this.getPackageName(), R.layout.fragment_main);
+            appWidgetManager.updateAppWidget(mAppWidgetId, views);
+
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    }
+}
